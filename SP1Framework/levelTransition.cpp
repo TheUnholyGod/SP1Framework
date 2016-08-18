1da72531a2bb76d175ca021bc3f0cea74decae2d
Calipao <calipaolx@gmail.com>
2016-08-18 15:45:49 +0800

    Updated maps to be compatible

--- SP1Framework/levelTransition.cpp
+++ SP1Framework/levelTransition.cpp
@@ -14,6 +14,35 @@ void checkGameGoal()
 			g_sChar.m_cLocation.Y = 10;
 			g_sChar.m_cLocation.X = 10;
 		}
-
+		if (g_CurrentLevel == 2 && (g_sChar.m_cLocation.Y == 2 && g_sChar.m_cLocation.X == 2))
+		{
+			g_CurrentLevel = 3;
+			g_sChar.m_cLocation.Y = 10;
+			g_sChar.m_cLocation.X = 10;
+		}
+		if (g_CurrentLevel == 3 && (g_sChar.m_cLocation.Y == 2 && g_sChar.m_cLocation.X == 2))
+		{
+			g_CurrentLevel = 4;
+			g_sChar.m_cLocation.Y = 10;
+			g_sChar.m_cLocation.X = 10;
+		}
+		if (g_CurrentLevel == 4 && (g_sChar.m_cLocation.Y == 2 && g_sChar.m_cLocation.X == 2))
+		{
+			g_CurrentLevel = 5;
+			g_sChar.m_cLocation.Y = 10;
+			g_sChar.m_cLocation.X = 10;
+		}
+		if (g_CurrentLevel == 5 && (g_sChar.m_cLocation.Y == 2 && g_sChar.m_cLocation.X == 2))
+		{
+			g_CurrentLevel = 6;
+			g_sChar.m_cLocation.Y = 10;
+			g_sChar.m_cLocation.X = 10;
+		}
+		if (g_CurrentLevel == 6 && (g_sChar.m_cLocation.Y == 2 && g_sChar.m_cLocation.X == 2))
+		{
+			g_CurrentLevel = 1;
+			g_sChar.m_cLocation.Y = 10;
+			g_sChar.m_cLocation.X = 10;
+		}
 	}
 }
