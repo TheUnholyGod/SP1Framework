2f6450275c24928a72920bbfd0c2a1605e457c1c
DESKTOP-VEJ40A3\Jadelays <jerrywong243@gmail.com>
2016-08-30 18:05:58 +0800

    improved

--- SP1Framework/smashableObjects.cpp
+++ SP1Framework/smashableObjects.cpp
@@ -12,7 +12,7 @@ void pickObtain()
 {
 	if (g_eGameState == S_GAME)
 	{
-		if (g_abKeyPressed[K_E] && map[g_sChar.m_cLocation.Y - 1][g_sChar.m_cLocation.X] == (char)156)
+		if (map[g_sChar.m_cLocation.Y - 1][g_sChar.m_cLocation.X] == (char)156)
 		{
 			g_PicksObtain += 4;
 			map[g_sChar.m_cLocation.Y - 1][g_sChar.m_cLocation.X] = (char)176;
@@ -22,7 +22,7 @@ void pickObtain()
 
 	if (g_eGameState == S_CREATIVE)
 	{
-		if (g_abKeyPressed[K_E] && map[g_sCreaChar.m_cCreativeLocation.Y - 1][g_sCreaChar.m_cCreativeLocation.X] == (char)156)
+		if (map[g_sCreaChar.m_cCreativeLocation.Y - 1][g_sCreaChar.m_cCreativeLocation.X] == (char)156)
 		{
 			g_PicksObtain += 4;
 			map[g_sCreaChar.m_cCreativeLocation.Y - 1][g_sCreaChar.m_cCreativeLocation.X] = (char)176;
